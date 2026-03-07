6018a0575c4e9d874b675839873f16cc356308fd
patricklubach <plubach1994@gmail.com>
2026-03-07 21:06:34 +0100

    feat: Add possibility to edit task
    that does not work (sadge)...

--- public/handle.js
+++ public/handle.js
@@ -1,39 +1,69 @@
-let tasks = [];
+function showError(err) {
+  const errorBox = document.getElementById("error");
 
-class Task {
-  constructor(title, description = undefined) {
-    this.id = crypto.randomUUID();
-    this.title = title;
-    this.description = description;
+  errorBox.textContent = err.message;
+  errorBox.style.display = "block";
+
+  console.error(err);
+}
+
+let tasks = new Map();
+
+function saveTasks() {
+  try {
+    console.log("Updating tasks");
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    console.log("Tasks list updated");
+  } catch (error) {
+    showError(error);
   }
 }
 
-function createTaskElement(id, title, description = "") {
-  const descriptionElement = description != "" ? `<p>${description}</p>` : "";
-  return `<div class="task" id="${id}">
-    ${descriptionElement}
-    <h3>${title}</h3>
-    <button class="done-button" onclick="toggle('${id}') ">Done</button>
-    <button class="delete-button" onclick="removeTask('${id}')">Delete</button>
-    </div>`;
+function loadTasks() {
+  console.log("Loading tasks");
+  let saved = localStorage.getItem("tasks");
+  if (saved !== null) {
+    tasks = saved;
+  }
 }
 
 function displayTasks() {
   console.log("Displaying tasks");
   let html = "";
-  for (let i = 0; i < tasks.length; i++) {
-    let task = tasks[i];
-    html += createTaskElement(task.id, task.title, title.description);
-  }
+  tasks.forEach(function (task, id) {
+    html += createTaskElement(task.id, task.title, task.description);
+  });
   document.getElementById("tasks").innerHTML = html;
 }
 
-function addTask() {
+class Task {
+  constructor(title, description = "") {
+    this.id = crypto.randomUUID().slice(0, 8);
+    this.title = title;
+    this.description = description;
+  }
+}
+
+function createTaskElement(id, title, description) {
+  console.log(`Create task, id: ${id}, title: ${title}, desc: ${description}`);
+  const descriptionElement =
+    description != "" ? `<p id="${id}-description">${description}</p>` : "";
+  return `<div class="task" id="${id}">
+    <h3 id="${id}-title">${title}</h3>
+    ${descriptionElement}
+    <button class="done-button" onclick="toggle('${id}') ">Done</button>
+    <button class="delete-button" onclick="removeTask('${id}')">Delete</button>
+    <button class="edit-button" onclick="editTask('${id}') ">Edit</button>
+  </div>`;
+}
+
+function createTask() {
   const title = document.getElementById("title").value;
   const description = document.getElementById("description").value;
-  console.log(`Adding task "${title}"`);
+  console.log(`Adding task: "${title}"`);
   const task = new Task(title, description);
-  tasks.push(task);
+  console.log(typeof tasks);
+  tasks.set(task.id, task);
 
   saveTasks();
   document.getElementById("title").value = "";
@@ -43,36 +73,49 @@ function addTask() {
 
 function removeTask(id) {
   console.log(`Removing task "${id}"`);
-  for (let i = 0; i < tasks.length; i++) {
-    let task = tasks[i];
-    if (task.id === id) {
-      tasks.splice(i, 1);
-    }
-  }
+  tasks.delete(id);
   saveTasks();
   displayTasks();
 }
 
-function saveTasks() {
-  console.log("Updating tasks");
-  localStorage.setItem("tasks", JSON.stringify(tasks));
-  console.log("Tasks list updated");
+function editTask(id) {
+  console.log(`Editing task "${id}"`);
+  const task = tasks.get(id);
+
+  let taskElement = document.getElementById(id);
+
+  taskElement.innerHTML = `
+    <input
+        id="${id}-title"
+        type="text"
+        required
+        value="${task.title}"
+    />
+    <input
+        id="${id}-description"
+        type="text"
+        required
+        value="${task.description}"
+    />
+    <button class="save-button" onclick="saveTask('${id}') ">Save</button>`;
 }
 
-// Function to Load tasks
-function loadTasks() {
-  console.log("Loading tasks");
-  let saved = localStorage.getItem("tasks");
-  if (saved !== null) {
-    tasks = JSON.parse(saved);
-  }
+function saveTask(id) {
+  console.log("Save tasks");
+
+  const task = tasks.get(id);
+  task.title = document.getElementById(`${id}-title`).value;
+  task.description = document.getElementById(`${id}-description`).value;
+
+  saveTasks();
+  displayTasks();
 }
 
 const titleInput = document.getElementById("title");
 titleInput.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
     e.preventDefault();
-    addTask();
+    createTask();
   }
 });
 
@@ -87,5 +130,19 @@ function toggle(id) {
   }
 }
 
-loadTasks();
-displayTasks();
+function init() {
+  localStorage.clear();
+  const task = new Task("foobar", "my cool desc");
+  tasks.set(task.id, task);
+  saveTasks();
+}
+
+try {
+  init();
+  loadTasks();
+  if (tasks.size > 0) {
+    displayTasks();
+  }
+} catch (err) {
+  showError(err);
+}
