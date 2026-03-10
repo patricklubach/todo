a06b3b95a94ab0d61155526d99fa5ba07f6785a6
patricklubach <plubach1994@gmail.com>
2026-03-10 20:39:01 +0100

    fix: Done button

--- public/handle.js
+++ public/handle.js
@@ -9,16 +9,6 @@ class Task {
     this.archived = false;
   }
 
-  save() {
-    try {
-      console.log(`Saving task (id: ${this.id})`);
-      localStorage.setItem(this.id, JSON.stringify(this));
-    } catch (error) {
-      showError(error);
-    }
-    console.log(`Task saved sucessfully (id: ${this.id})`);
-  }
-
   getHTMLElement() {
     const taskElement = document.createElement("div");
     taskElement.id = this.id;
@@ -27,6 +17,9 @@ class Task {
     const descriptionElement = document.createElement("p");
     descriptionElement.id = this.id;
     descriptionElement.innerHTML = this.description;
+    if (this.done) {
+      descriptionElement.classList.add("done")
+    }
     taskElement.appendChild(descriptionElement);
 
     const btnWrapperElement = document.createElement("div");
@@ -35,8 +28,12 @@ class Task {
 
     const doneButton = document.createElement("button");
     doneButton.classList.add("done-button");
-    doneButton.onclick = () => toggle(this.id);
-    doneButton.textContent = "Done";
+    doneButton.onclick = () => Tasks.toggleDone(this.id);
+    if (this.done) {
+      doneButton.textContent = "Undone";
+    } else {
+      doneButton.textContent = "Done";
+    }
     btnWrapperElement.appendChild(doneButton);
 
     const deleteButton = document.createElement("button");
@@ -53,9 +50,80 @@ class Task {
 
     return taskElement;
   }
+
+  save() {
+    try {
+      console.log(`Saving task (id: ${this.id})`);
+      localStorage.setItem(this.id, JSON.stringify(this));
+    } catch (error) {
+      showError(error);
+    }
+    console.log(`Task saved sucessfully (id: ${this.id})`);
+  }
+
+  static toggle(id) {
+    const task = Tasks.load(id)
+
+    const taskElement = document.getElementById(id);
+    const description = taskElement.querySelector("p");
+    description.classList.toggle("done");
+  }
 }
 
 class Tasks {
+  static add() {
+    const description = document.getElementById("description").innerHTML;
+    this.create(description);
+  }
+
+  static create(description) {
+    console.log(`Creating task (description: "${description}")`);
+    const task = new Task(description);
+    console.log(`Created task (id: ${task.id})`);
+    task.save();
+    
+    document.getElementById("description").innerHTML = "";
+    this.display();
+  }
+  
+  static delete(id) {
+    console.log(`Deleting task (id: ${id})`);
+    localStorage.removeItem(id);
+    console.log(`Task deleted sucessfully (id: ${id})`);
+    this.display();
+  }
+  
+  static display(tasks = []) {
+    console.log("Displaying tasks");
+    const tasksElement = document.getElementById("tasks");
+    
+    let newTasksElement = document.createElement("div");
+    newTasksElement.id = "tasks";
+    
+    let tasksList;
+    if (0 < tasks.length) {
+      tasksList = tasks;
+    } else {
+      tasksList = this.loadAll();
+    }
+    tasksList.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+    
+    for (let task of tasksList) {
+      let taskElement = task.getHTMLElement();
+      newTasksElement.appendChild(taskElement);
+    }
+    
+    tasksElement.replaceWith(newTasksElement);
+  }
+  
+  static edit(id) {
+    console.log(`Editing task "${id}"`);
+    const task = Tasks.load(id);
+    task.editMode = true;
+    task.save();
+    Tasks.display();
+  }
+  
   static load(id) {
     const data = JSON.parse(localStorage.getItem(id));
     const task = Object.assign(new Task(), data);
@@ -73,64 +141,12 @@ class Tasks {
     return tasksList;
   }
 
-  static add() {
-    const description = document.getElementById("description").innerHTML;
-    this.create(description);
-  }
-
-  static delete(id) {
-    console.log(`Deleting task (id: ${id})`);
-    localStorage.removeItem(id);
-    console.log(`Task deleted sucessfully (id: ${id})`);
+  static toggleDone(id) {
+    const task = this.load(id);
+    task.done ? task.done = false : task.done = true;
+    task.save();
     this.display();
   }
-
-  static display(tasks = []) {
-    console.log("Displaying tasks");
-    const tasksElement = document.getElementById("tasks");
-
-    let newTasksElement = document.createElement("div");
-    newTasksElement.id = "tasks";
-
-    let tasksList;
-    if (0 < tasks.length) {
-      tasksList = tasks;
-    } else {
-      tasksList = this.loadAll();
-    }
-    tasksList.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
-    
-    for (let task of tasksList) {
-      let taskElement = task.getHTMLElement();
-      newTasksElement.appendChild(taskElement);
-    }
-    
-    tasksElement.replaceWith(newTasksElement);
-  }
-
-  static create(description) {
-    console.log(`Creating task (description: "${description}")`);
-    const task = new Task(description);
-    console.log(`Created task (id: ${task.id})`);
-    task.save();
-
-    document.getElementById("description").innerHTML = "";
-    this.display();
-  }
-
-  static edit(id) {
-    console.log(`Editing task "${id}"`);
-    const task = Tasks.load(id);
-    task.editMode = true;
-    task.save();
-    Tasks.display();
-  }
-}
-
-function toggle(id) {
-  const taskElement = document.getElementById(id);
-  const description = taskElement.querySelector("p");
-  description.classList.toggle("done");
 }
 
 function init() {
